d4d4fff8582aa82efeed34ac4b60216d26c32ef4
Alankrita Patidar <2102561>
2022-12-08 12:44:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//const http = require('http')
+
+const express= require('express');
+
+const app=express();
+
+app.use((req,res,next) =>{
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req,res,next) =>{
+  console.log("In the another middleware");
+  res.send(`<h1>Hello from Express.Js!!</h1>`);
+})
+app.listen(3000);
+
+/*const server=http.createServer(app);
+
+server.listen(3000);*/
